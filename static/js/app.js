20461e32f0e4fbe13562e3c731decdfc1d631baf
Elize Papineau <elizepapineau@gmail.com>
2018-11-02 17:08:07 -0700

    added level 2

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,10 @@ var tableData = data;
 
 // Define pieces of site
 var date = d3.select("#datetime");
+var city = d3.select("#city-input");
+var state = d3.select("#state-input");
+var country = d3.select("#country-input");
+var shape = d3.select("#shape-input");
 var button = d3.select("#filter-btn");
 
 button.on("click", function(){
@@ -13,6 +17,14 @@ button.on("click", function(){
     var searchDate = date.property("value");
     searchDate = new Date(searchDate);
 
+    // Format remaining search terms
+    searchCity = city.property("value").toLowerCase();
+    searchState = state.property("value").toLowerCase().replace(" ", "");
+    searchCountry = country.property("value").toLowerCase().replace(" ", "");
+    searchShape = shape.property("value").toLowerCase().replace(" ", "");
+
+    console.log(searchCity, searchState, searchCountry, searchShape);
+
     // Setup table variable
     var tBody = d3.select("tbody");
 
@@ -21,7 +33,12 @@ button.on("click", function(){
         // Format date string for matching
         var dataDate = new Date(sighting.datetime)
 
-        if(dataDate.getTime() === searchDate.getTime()){
+        if(dataDate.getTime() === searchDate.getTime() &&
+            sighting.city === searchCity &&
+            sighting.state === searchState &&
+            sighting.country === searchCountry &&
+            sighting.shape === searchShape){
+                
             // Append matches to table
             tBody.append("tr");
             tBody.append("td").text(sighting.datetime);
